e07c5ff053bcd28d242699afe9648d66aac1dc12
djxcds <24988113+djxcds@users.noreply.github.com>
2022-09-20 21:23:30 +0800

    updated api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   appVersion: require('../../package.json').version,
-  apiUrl: 'https://ntc-10-law-office.herokuapp.com',
+  apiUrl: 'https://ntc-tms.onrender.com',
   production: true,
   appLogo: '/assets/images/logo.png',
 };
